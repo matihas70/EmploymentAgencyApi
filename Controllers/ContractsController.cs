242b51802ae42f932bd7eca40b7b74f9d60de121
Mateusz Hasik <matihasek@gmail.com>
2022-06-24 11:35:35 +0200

    Added controller for contracts

---
+++ Controllers/ContractsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmploymentAgencyApi.Controllers
+{
+    public class ContractsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
